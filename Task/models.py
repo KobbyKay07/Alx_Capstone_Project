b87e1615ba637e4b97f6efd7eb5c26b9138f796d
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-07 23:33:49 +0000

    Implementing djando models

--- Task/models.py
+++ Task/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=128)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
+    
+class tasks(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(users, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.title
