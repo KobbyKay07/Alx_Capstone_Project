fa96f5cdbf5df7f0a2d09e2d61d8a26f0ac9578c
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-07 23:59:10 +0000

    implement the core task endpoints (CRUD)

--- Task/views.py
+++ Task/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import generics
+from .models import tasks
+from .serializers import TaskSerializer
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Welcome to Task Management API")
+
+# Create & List Tasks 
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = tasks.objects.all()
+    serializer_class = TaskSerializer
+
+# Retrieve, Update & Delete Task
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = tasks.objects.all()
+    serializer_class = TaskSerializer    
