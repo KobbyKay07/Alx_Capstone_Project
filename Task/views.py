82baf65c243f11e1b15a659d50bcaec5004a5658
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-14 19:29:50 +0000

    Included user endpoints for CRUD operations

--- Task/views.py
+++ Task/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
-from .models import tasks
-from .serializers import TaskSerializer
+from .models import Users, Tasks
+from .serializers import TaskSerializer, UserSerializer
 
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to Task Management API")
 
+# Create & List Users
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UserSerializer
+
+# Retrieve, Update & Delete User
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UserSerializer
+
 # Create & List Tasks 
 class TaskListCreateView(generics.ListCreateAPIView):
-    queryset = tasks.objects.all()
+    queryset = Tasks.objects.all()
     serializer_class = TaskSerializer
 
 # Retrieve, Update & Delete Task
 class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = tasks.objects.all()
+    queryset = Tasks.objects.all()
     serializer_class = TaskSerializer    
