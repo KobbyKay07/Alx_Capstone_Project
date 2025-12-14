d0985248e8f50dd4caec905ba0ca9fcaf0c58e3d
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-14 19:19:57 +0000

    Changed (users,tasks) to (Users,Tasks)

--- Task/models.py
+++ Task/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class users(models.Model):
+class Users(models.Model):
     username = models.CharField(max_length=150, unique=True)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=128)
@@ -11,13 +11,13 @@ class users(models.Model):
     def __str__(self):
         return self.username
     
-class tasks(models.Model):
+class Tasks(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(users, on_delete=models.CASCADE, related_name='tasks')
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='tasks')
 
     def __str__(self):
         return self.title
