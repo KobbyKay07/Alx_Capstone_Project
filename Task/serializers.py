d1bfe6dd720d8d6430536e3ec81d7d748ebaf0b2
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-14 19:14:45 +0000

    Added a serializer for the user model

--- Task/serializers.py
+++ Task/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import tasks
+from .models import Tasks, Users
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ["id", "username", "email", "date_joined", "is_active"]
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
-        model = tasks
+        model = Tasks
         fields = "__all__"
