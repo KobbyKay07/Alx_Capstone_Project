cb3fb3ec252d82f65bfa0f7793e2962be922dfa5
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-14 19:35:41 +0000

    Added Urls for user endpoints

--- Task/urls.py
+++ Task/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import TaskListCreateView, TaskDetailView
+from .views import TaskListCreateView, TaskDetailView, UserListCreateView, UserDetailView
 
 urlpatterns = [
+    # User endpoints
+    path('users/', UserListCreateView.as_view(), name='user_list_create'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+    # Task endpoints
     path('tasks/', TaskListCreateView.as_view(), name='task_list_create'),
     path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
 ]
