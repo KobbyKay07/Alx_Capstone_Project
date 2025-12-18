3b1557dc6daea091598e06d06ffcb302305c8cd3
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-18 14:12:35 +0000

    Implemented views to mark tasks pending, in-progress or complete

--- Task/views.py
+++ Task/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from django.utils import timezone
 from django.http import HttpResponse
 from rest_framework import generics
 from .models import Users, Tasks
@@ -7,6 +12,49 @@ from .serializers import TaskSerializer, UserSerializer
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to Task Management API")
+
+def mark_task_pending(request,pk):
+    try:
+        task = Tasks.objects.get(pk=pk, user=request.user)
+    except Tasks.DoesNotExist:
+        return Response({"error": "Task not found."}, status=status.HTTP_404_NOT_FOUND)
+
+    task.is_completed = False
+    task.completed_at = None
+    task.status = 'pending'
+    task.save()
+
+    return Response(TaskSerializer(task).data)
+
+@api_view(["PATCH"])
+@permission_classes([IsAuthenticated])
+def mark_task_in_progress(request, pk):
+    try:
+        task = Tasks.objects.get(pk=pk, user=request.user)
+    except Tasks.DoesNotExist:
+        return Response({"error": "Task not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    task.status = "in_progress"
+    task.is_completed = False
+    task.completed_at = None
+    task.save()
+    
+    return Response(TaskSerializer(task).data)
+
+@api_view(['PATCH'])
+@permission_classes([IsAuthenticated])
+def mark_task_completed(request,pk):
+    try:
+        task = Tasks.objects.get(pk=pk, user=request.user)
+    except Tasks.DoesNotExist:
+        return Response({"error": "Task not found."}, status=status.HTTP_404_NOT_FOUND)
+
+    task.is_completed = True
+    task.completed_at = timezone.now()
+    task.status = 'completed'
+    task.save()
+
+    return Response(TaskSerializer(task).data)
 
 # Create & List Users
 class UserListCreateView(generics.ListCreateAPIView):
