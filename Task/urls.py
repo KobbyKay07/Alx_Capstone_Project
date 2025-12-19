4bca14e96174dd348517038f911646a06cbadaaf
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-19 07:53:55 +0000

    Added urls for task status transition endpoints

--- Task/urls.py
+++ Task/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     # Task endpoints
     path('tasks/', TaskListCreateView.as_view(), name='task_list_create'),
     path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
+    # Task status transition endpoints
     path("tasks/<int:pk>/pending/", mark_task_pending, name="task-pending"),
     path("tasks/<int:pk>/in-progress/", mark_task_in_progress, name="task-in-progress"),
-    path("tasks/<int:pk>/complete/", mark_task_complete, name="task-complete"),
+    path("tasks/<int:pk>/complete/", mark_task_complete, name="task-complete")
 ]
