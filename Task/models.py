445693318833328cd7792d053e020107f310cc6e
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-19 07:05:16 +0000

    Changed my Users model to CustomUser to extend AbstractUser

--- Task/models.py
+++ Task/models.py
@@ -1,12 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
-class Users(models.Model):
-    username = models.CharField(max_length=150, unique=True)
-    email = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=128)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
 
     def __str__(self):
         return self.username
@@ -33,7 +33,7 @@ class Tasks(models.Model):
     completed_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='tasks')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tasks')
 
     def __str__(self):
         return self.title
