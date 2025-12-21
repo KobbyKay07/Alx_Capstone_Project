0653329a13bbe408d7f22ca530821f4837f7bae0
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-21 17:05:12 +0000

    Added User registration serializer and modified user serializer

--- Task/serializers.py
+++ Task/serializers.py
@@ -8,19 +8,23 @@ from datetime import timedelta
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
         fields = ["id", "username", "email", "date_joined", "is_active"]
 
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "password"]
+
     def create(self, validated_data):
-        user = User.objects.create_user(
+        return User.objects.create_user(
             username=validated_data["username"],
-            email=validated_data["email"],
-            password=validated_data["password"],
+            email=validated_data.get("email"),
+            password=validated_data.get("password"),
         )
-        return user
     
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
