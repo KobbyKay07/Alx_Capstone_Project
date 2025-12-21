7808125a78c8c92b67ad41d922db63102a43f3c1
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-21 14:12:50 +0000

    Writing unittest for Taskserializer

--- Task/tests.py
+++ Task/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .models import Tasks
+from .serializers import TaskSerializer
 
 # Create your tests here.
 User = get_user_model()
@@ -19,3 +20,12 @@ class TaskModelTest(TestCase):
         self.assertEqual(self.task.title, "Test Task")
         self.assertEqual(self.task.status, "pending")
         self.assertEqual(self.task.user, self.user)
+
+class TaskSerializerTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email="test@example.com", password="pass123")
+        self.task = Tasks.objects.create(title="Test", user=self.user)
+
+    def test_valid_serializer(self):
+        serializer = TaskSerializer(instance=self.task)
+        self.assertEqual(serializer.data["title"], "Test")
