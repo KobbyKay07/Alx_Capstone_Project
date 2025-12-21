d8b6fd53129d4754c1c1a762526f8d4f076ae00e
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-21 14:08:01 +0000

    Writing unittests for models

--- Task/tests.py
+++ Task/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Tasks
 
 # Create your tests here.
+User = get_user_model()
+
+class TaskModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email="test@example.com", password="pass123")
+        self.task = Tasks.objects.create(
+            title="Test Task",
+            description="Testing",
+            user=self.user,
+            status="pending"
+        )
+
+    def test_task_creation(self):
+        self.assertEqual(self.task.title, "Test Task")
+        self.assertEqual(self.task.status, "pending")
+        self.assertEqual(self.task.user, self.user)
