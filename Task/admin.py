ab46d0fc689adba245d5417c408297625c6e33f1
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-21 11:29:14 +0000

    Registered models in admin file

--- Task/admin.py
+++ Task/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Tasks, TaskHistory
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(TaskHistory)
+admin.site.register(Tasks)
