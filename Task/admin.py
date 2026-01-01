69cf3090c6d5e779abf57581a9ba5cd90bf4a97a
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2026-01-01 09:51:13 +0000

    Added a customuseradmin

--- Task/admin.py
+++ Task/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Category, Tasks, TaskHistory, CustomUser, Notification
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'is_staff', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone_number', 'profile_picture')}),
+    )
 
 admin.site.register(Category)
 admin.site.register(TaskHistory)
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Notification)
 admin.site.register(Tasks)
